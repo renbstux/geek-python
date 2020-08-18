c31efdc836fffa7b42e815de2aee2190f348010a
Renato de Souza <renbstux@gmail.com>
2020-08-18 20:30:06 -0300

    Exercises Section 16

---
+++ Exercise_Python_Section_16.py
@@ -0,0 +1,18 @@
+"""
+1 - Crie uma classe para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os
+métodos públicos necessariso para sets e gets e também um método para imprimir os dados de uma pessoa.
+
+class Pessoa:
+    def __init__(self, nome, idade, altura):
+        self.__nome = nome
+        self.__idade = idade
+        self.__altura = altura
+
+    def dados_pessoa(self):
+        print(f'O/A {self.__nome} tem a idade de: {self.__idade} anos e tem uma Altura de {self.__altura} cm!')
+
+p1 = Pessoa('Renato de Souza', 44, 1.81)
+
+p1.dados_pessoa()
+"""
+
