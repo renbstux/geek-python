38cd063d1dc111bbfb7187da9152b2579ec61562
Renato de Souza <renbstux@gmail.com>
2020-09-14 22:29:19 -0300

    Single Thread, Multi-Thread and Multiprocessing

---
+++ aula02.py
@@ -0,0 +1,17 @@
+"""
+idade = 44
+
+print(type(idade))
+
+idade = 'Quarenta e Quatro'
+
+print(type(idade))
+
+if True:
+    resultado = 1 + 'Geek' # PERIGO
+else:
+    resultado = 1 + 41
+
+print(resultado)
+
+"""
